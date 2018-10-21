74366f8939c60ebdc5a2b8a382a5da1732a3ad7e
Rodrigo Andrades <rodrigomail2007@gmail.com>
2018-10-20 22:09:42 -0300

    dockerize

--- mock-device/index.js
+++ mock-device/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs'); 
 var jwt = require('jsonwebtoken'); 
 var mqtt = require('mqtt'); 
--- monitor-service/index.js
+++ monitor-service/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('dotenv').config()
 
 const PubSub = require('@google-cloud/pubsub');
