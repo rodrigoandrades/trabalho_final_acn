373a0753a680f91d0ef2dfc8c039c9446a50d962
Rodrigo Andrades <rodrigomail2007@gmail.com>
2018-10-20 21:29:17 -0300

    monitor server

--- monitor-service/index.js
+++ monitor-service/index.js
@@ -1,36 +1,26 @@
-// Imports the Google Cloud client library
+require('dotenv').config()
+
 const PubSub = require('@google-cloud/pubsub');
 
-// Your Google Cloud Platform project ID
 const projectId = 'eighth-jigsaw-219600';
+const subscriberName = 'my-sub';
 
-// Instantiates a client
 const pubsub = new PubSub({
   projectId: projectId,
 });
 
-var topic = pubsub.topic('my-topic');
-var subscription = topic.subscription('my-sub');
+var subscription = pubsub.subscription('projects/' + projectId + '/subscriptions/' + subscriberName); 
 
-subscription.on('error', function(err) {
-    console.log('error')
-});
-
-function onMessage(message) {
-  // Called every time a message is received.
-
-  console.log(message);
-  // message.id = ID of the message.
-  // message.ackId = ID used to acknowledge the message receival.
-  // message.data = Contents of the message.
-  // message.attributes = Attributes of the message.
-  // message.timestamp = Timestamp when Pub/Sub received the message.
-
-  // Ack the message:
-  message.ack();
-
-  // This doesn't ack the message, but allows more messages to be retrieved
-  // if your limit was hit or if you don't want to ack the message.
-  // message.nack();
-}
-subscription.on('message', onMessage);
+var messageHandler = function(message) { 
+  console.log('Message Begin >>>>>>>>'); 
+  console.log('message.connectionId', message.connectionId); 
+  console.log('message.attributes', message.attributes); 
+  console.log('message.data', Buffer.from(message.data, 'base64').toString('ascii')); 
+  console.log('Message End >>>>>>>>>>'); 
+ 
+  // "Ack" (acknowledge receipt of) the message 
+  message.ack(); 
+}; 
+ 
+// Listen for new messages 
+subscription.on('message', messageHandler);
