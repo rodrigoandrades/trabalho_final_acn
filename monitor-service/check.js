9bbe0129b49530098075972c6da9edf413ac5a32
Rodrigo Andrades <rodrigomail2007@gmail.com>
2018-10-22 21:24:54 -0300

    check

--- monitor-service/check.js
+++ monitor-service/check.js
@@ -3,3 +3,8 @@ let check = (dados) => {
 }
 
 module.exports = check
+
+/*
+A frequência cardíaca normal, em repouso, varia entre 60 e 100 batimentos por minuto (bpm). Quando os batimentos cardíacos estão acelerados, acima de 100 bpm, a pessoa está com taquicardia. Uma frequência cardíaca baixa, inferior a 60 bpm, é considerada bradicardia.
+Quando esta pressão está acima de 14/9 a pressão é considerada alta, quando está abaixo de 10/6 os valores são considerados baixos
+*/
